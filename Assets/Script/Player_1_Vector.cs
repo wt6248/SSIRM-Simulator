63b3ced4106be9c31f45e4db25fac20df9024bbb
Song Si Ho <wt6248@gmail.com>
2020-01-30 13:19:23 +0900

    First test git commit. vector develop unfinished.

---
+++ Assets/Script/Player_1_Vector.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_1_Vector : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float vector_x;
+    float vector_y;
+    public GameObject vector_Manager;
+    void Start()
+    {
+        vector_x = 0f;
+        vector_y = 0f;
+       // vector_Manager = GetComponent<VectorManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change_vector();
+        make_transform_from_vectors();
+    }
+
+    void change_vector()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            vector_x -= 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            vector_x += 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            vector_y += 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            vector_y -= 1f;
+        }
+    }
+
+    public float vector_length()
+    {
+        return Mathf.Pow((Mathf.Pow(vector_x,2)+ Mathf.Pow(vector_y, 2)), 0.5f);
+    }
+
+    void make_transform_from_vectors()
+    {
+        float temp = Mathf.Atan2(vector_y, vector_x) * Mathf.Rad2Deg;
+        float scale = vector_length()/20;
+        float x_trans = Mathf.Cos(Mathf.Atan2(vector_y, vector_x)) * scale * 4;
+        float Y_trans = Mathf.Sin(Mathf.Atan2(vector_y, vector_x)) * scale * 4;
+        transform.position = new Vector3(x_trans, Y_trans, 0f);
+        transform.rotation = Quaternion.Euler(0,0,temp);
+        transform.localScale = new Vector3(scale, scale, 0);
+
+    }
+}
---
+++ Assets/Script/VectorManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class VectorManager : MonoBehaviour
+{
+
+    public float click_Change = 1f;
+    public float high_Click_Change = 0.7f;
+    public float low_Click_Change = 1.3f;
+
+    public GameObject player_Vector_1;
+    public GameObject player_Vector_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void change_Change()
+    {
+        click_Change *= 1.5f;
+        high_Click_Change *= 1.5f;
+        low_Click_Change *= 1.5f;
+    }
+}
