978f71f75489be610a59782d7199b61fdfe23180
Song Si Ho <wt6248@gmail.com>
2020-01-30 14:08:13 +0900

    1. 플레이어 2 백터 추가. 2. 백터 매니저 내용 작성 3. 미구현 사항들 : 상대의 백터 줄이기, 게임종료 하기, 백터 위치 코드 수정, 백터 길이 수정

--- Assets/Script/Player_2_Vector.cs
+++ Assets/Script/Player_2_Vector.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_1_Vector : MonoBehaviour
+public class Player_2_Vector : MonoBehaviour
 {
     // Start is called before the first frame update
 
@@ -27,19 +27,19 @@ public class Player_1_Vector : MonoBehaviour
 
     void change_vector()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             vector_x -= change_reference.click_Change;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             vector_x += change_reference.click_Change;
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             vector_y += change_reference.click_Change;
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             vector_y -= change_reference.click_Change;
         }
@@ -47,22 +47,17 @@ public class Player_1_Vector : MonoBehaviour
 
     public float vector_length()
     {
-        return Mathf.Pow((Mathf.Pow(vector_x,2)+ Mathf.Pow(vector_y, 2)), 0.5f);
-    }
-
-    public float vector_Angle()
-    {
-        return Mathf.Atan2(vector_y, vector_x);
+        return Mathf.Pow((Mathf.Pow(vector_x, 2) + Mathf.Pow(vector_y, 2)), 0.5f);
     }
 
     void make_transform_from_vectors()
     {
         float temp = Mathf.Atan2(vector_y, vector_x) * Mathf.Rad2Deg;
-        float scale = vector_length()/20;
+        float scale = vector_length() / 20;
         float x_trans = Mathf.Cos(Mathf.Atan2(vector_y, vector_x)) * scale * 4;
         float Y_trans = Mathf.Sin(Mathf.Atan2(vector_y, vector_x)) * scale * 4;
         transform.position = new Vector3(x_trans, Y_trans, 0f);
-        transform.rotation = Quaternion.Euler(0,0,temp);
+        transform.rotation = Quaternion.Euler(0, 0, temp);
         transform.localScale = new Vector3(scale, scale, 0);
 
     }
--- Assets/Script/VectorManager.cs
+++ Assets/Script/VectorManager.cs
@@ -14,16 +14,19 @@ public class VectorManager : MonoBehaviour
     public GameObject player_Vector_1;
     public GameObject player_Vector_2;
 
+    Player_1_Vector function_Reference_1;
+    Player_2_Vector function_Reference_2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        function_Reference_1 = player_Vector_1.GetComponent<Player_1_Vector>();
+        function_Reference_2 = player_Vector_2.GetComponent<Player_2_Vector>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        winner_checker();
     }
 
     public void change_Change()
@@ -31,5 +34,36 @@ public class VectorManager : MonoBehaviour
         click_Change *= 1.5f;
         high_Click_Change *= 1.5f;
         low_Click_Change *= 1.5f;
+    }
+
+    public void winner_checker()
+    {
+        float length_1 = function_Reference_1.vector_length();
+        float length_2 = function_Reference_2.vector_length();
+        if(length_1 > 180f)
+        {
+            //1이 이겼다는 내용과 함께 게임종료 함수를 호출
+        }
+        if (length_2 > 180f)
+        {
+            // 2가 이겼다는 내용과 함께 게임종료 함수 호출
+        }
+    }
+
+    public int winner_Decider()
+    {
+        float length_1 = function_Reference_1.vector_length();
+        float length_2 = function_Reference_2.vector_length();
+
+        if (length_1 > length_2)
+        {
+            return 1;
+        }
+        else if (length_2 > length_1)
+        {
+            return 2;
+        }
+        else
+            return 0;
     }
 }
