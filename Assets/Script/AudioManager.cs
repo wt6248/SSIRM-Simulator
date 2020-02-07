7b2e07cc47aa444ac65336aee2a2bcdd43b31be8
Gong Minsu <gmsgms0606@gmail.com>
2020-02-07 21:56:59 +0900

    Issue #2 배경 음악 추가
    변경점
    AudioManager 오브젝트 추가
    AudioManager 스크립트 추가
    배경음악(SSirm King (Cyber).mp3) 추가

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource source;
+    public AudioClip backgroundMusic;           //Inspector 에서 지정, 현재 : SSirm King (Cyber)
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        StartBGM();
+    }
+
+    public void StartBGM()
+    {
+        source.clip = backgroundMusic;
+        source.loop = true;
+        source.volume = 0.3f;
+        source.Play();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
