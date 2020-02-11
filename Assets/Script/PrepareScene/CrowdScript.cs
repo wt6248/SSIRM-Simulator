fed416b789746d524dcb620283d253a3a52a7789
Song Si Ho <wt6248@gmail.com>
2020-02-11 18:20:03 +0900

    게임 시작전에 심판이 일어서고, 징 울려주고, 선수 일어서는 장면 드렸습니다.

---
+++ Assets/Script/PrepareScene/CrowdScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdScript : MonoBehaviour
+{
+    bool movement;
+    IEnumerator stop_move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = true;
+        stop_move = UpDownMovement();
+        StartCoroutine(stop_move);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void stop_crowd_move()
+    {
+        StartCoroutine(stop_move);
+    }
+
+    IEnumerator UpDownMovement()
+    {
+
+        while (true)
+        {
+            Debug.Log("a");
+            yield return new WaitForSeconds(1);
+            if (movement)
+            {
+                transform.position += new Vector3(0, 0.3f, 0);
+                movement = false;
+            }
+            else
+            {
+                transform.position -= new Vector3(0, 0.3f, 0);
+                movement = true;
+            }
+        }
+    }
+}
