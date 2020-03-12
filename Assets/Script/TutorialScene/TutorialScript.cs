5e7fb09e65f512675a7a37563bceaec86f1f2493
Song Si Ho <wt6248@gmail.com>
2020-03-12 17:32:16 +0900

    #54 튜토리얼 씬 알파 구현함.
    1. 잘못 import했던 TextMesh Pro를 삭제함. (차후 예상치 못한 문제가 생길 수 있으나, 이는 TextMesh Pro를 Import 하는 것으로 해결할 수 있다고 생각한다.)
    2. 사용자가 방향키를 입력하는 튜토리얼 장면을 넣었음.
         2-1. 플레이어가 벡터를 원 바깥까지 늘이는 장면.
         2-2. 플레이어가가 상대방의 백터를 줄이는 장면.
    3. 튜토리얼 기획을 개똥같이 해서 확장성이 떨어짐. 튜토리얼을 갈아 엎는다면 나중에 누가 수정하면 좋을듯.
    4. 텍스트 박스가 작아서, 텍스트가 안보이는 현상이 일어나나, 텍스트 박스 및 텍스트의 크기 조절이 정해지면, 튜토리얼 스크립트를 수정하는 것으로 해결보려 함.

--- Assets/Script/TutorialScene/TutorialScript.cs
+++ Assets/Script/TutorialScene/TutorialScript.cs
@@ -7,26 +7,56 @@ public class TutorialScript : MonoBehaviour
     public UnityEngine.UI.Text tutorial_explanation;
     public GameObject ring_outside;
     public GameObject ring_inside;
+    public GameObject ring_whole;
+
+    public VectorManageScript vectorManageScript;
 
     int TutorialState;
+    bool tutorial_8_reseted;
+    bool tutorial_9_reseted;
     // Start is called before the first frame update
     void Start()
     {
-        TutorialState = 0;
-        ring_outside.SetActive(false);
-        ring_inside.SetActive(false);
-
-
+        TutorialState = 1;
+        tutorial_8_reseted = false;
+        tutorial_9_reseted = false;
+        ring_whole.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+
+
+        if (TutorialState == 5)
         {
-            TutorialState += 1;
-            Invoke("getScript_" + TutorialState.ToString(), 0f);
+            if (vectorManageScript.check_winning() == 1)
+                TutorialState += 1;
         }
+        else if (TutorialState == 6)
+        {
+            if (vectorManageScript.check_winning() == 2)
+                TutorialState += 1;
+        }
+        else if (TutorialState == 8)
+        {
+            if (vectorManageScript.check_p2_length_0())
+                TutorialState += 1;
+        }
+        else if (TutorialState == 9)
+        {
+            if (vectorManageScript.check_p1_length_0())
+                TutorialState += 1;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                TutorialState += 1;
+            }
+        }
+        Invoke("getScript_" + TutorialState.ToString(), 0f);
+
 
 
     }
@@ -46,37 +76,84 @@ public class TutorialScript : MonoBehaviour
     }
     void getScript_3()
     {
-        tutorial_explanation.text = "방향키(W,A,S,D 혹은 위,아래,왼쪽,오른쪽)를 연타하는 것으로 해당 부위에 힘을 줄 수 있다.";
+        ring_whole.SetActive(true);
+        tutorial_explanation.text = "각 선수는 방향키를 연타하는 것으로 신체의 각 부위에 힘을 줄 수 있다.\n" +
+                                    "자기가 준 힘의 정도는 이 씨름판의 화살표로 알 수 있지.\n" +
+                                    "화살표가 씨름판을 넘어갈 정도로 힘을 주면, 기술을 발동하면서 상대방을 제압할 수 있다.";
     }
     void getScript_4()
     {
-        ring_outside.SetActive(true);
-        ring_inside.SetActive(true);
-        tutorial_explanation.text = "자기가 준 힘의 정도는 이 씨름판의 화살표로 알 수 있지.";
+        tutorial_explanation.text = "각 방향키는 밀기, 당기기, 왼다리, 오른다리에 일치한다.\n" +
+                                    "1번 선수 홍샅바는 W, S, A, D를 연타하는 것으로 힘을 줄 수 있고,\n" +
+                                    "2번 선수 청샅바는 위, 아래, 왼쪽 오른쪽 방향키를 연타하는 것으로 힘을 줄 수 있다.";
     }
     void getScript_5()
     {
-        tutorial_explanation.text = "화살표가 씨름판 바깥으로 나가면, 기술이 발동해서 상대방을 넘어트릴 수 있다.";
+        tutorial_explanation.text = "자유롭게 입력해서 한번 확인해보도록. \n" +
+                                    "1번 선수 홍샅바의 화살표가 씨름판을 넘어가도록 해봐라.";
+
+        vectorManageScript.vector_1_enabled = true;
+        vectorManageScript.vector_2_enabled = false;
+        vectorManageScript.reset_game_p2();
     }
     void getScript_6()
     {
-        tutorial_explanation.text = "다만, 상대방 화살표 방향의 키를 입력해서, 상대방의 힘(화살표)을 빼는 것으로 방어할 수 있다.";
+        tutorial_explanation.text = "이번엔 2번 선수 청샅바가 화살표가 씨름판을 넘어가도록 해봐라.";
+
+        vectorManageScript.vector_1_enabled = false;
+        vectorManageScript.vector_2_enabled = true;
+        vectorManageScript.reset_game_p1();
     }
     void getScript_7()
     {
-        tutorial_explanation.text = "공격과 방어를 통해, 상대방을 넘어트리면 승리를 가질 수 있다.";
+        vectorManageScript.reset_game();
+        tutorial_explanation.text = "화살표가 씨름판 바깥으로 나가면, 기술이 발동해서 상대방을 넘어트릴 수 있다.\n" +
+                                    "상대방 화살표 방향의 키를 입력해서, 상대방의 힘(화살표)을 빼는 것으로 방어할 수 있다.";
     }
     void getScript_8()
     {
-        tutorial_explanation.text = "시간이 0이 되면 데스매치가 된다. 추가 시간이 주어지고, 더 강력하게 공격할 수 있지. \n추가 시간 동안에도 승부를 보지 못하면, 힘을 더 많이 가한 사람이 판정 승을 챙긴다.";
+        tutorial_explanation.text = "1번 선수 홍샅바가, 상대방의 공격을 방어해서 상대방의 화살표를 0으로 없애보도록.";
+
+        if (!tutorial_8_reseted)
+        {
+            vectorManageScript.reset_game_2();
+            tutorial_8_reseted = true;
+        }
+        vectorManageScript.vector_1_enabled = true;
+        vectorManageScript.vector_2_enabled = false;
+    }
+    void getScript_9()
+    {
+        tutorial_explanation.text = "이번에는 2번 선수 청샅바가, 상대방의 공격을 방어해서 상대방의 화살표를 없애보도록.";
+
+        if (!tutorial_9_reseted)
+        {
+            vectorManageScript.reset_game_3();
+            tutorial_9_reseted = true;
+        }
+        vectorManageScript.vector_1_enabled = false;
+        vectorManageScript.vector_2_enabled = true;
+
+    }
+    void getScript_10()
+    {
+
+        vectorManageScript.vector_1_enabled = true;
+        tutorial_explanation.text = "지금까지 알려준대로 공격과 방어를 통해, 상대방을 넘어트리면 승리를 가질 수 있다.\n" +
+                                    "자신의 화살표가 클수록, 상대방의 방어력이 더 커져서, 한번에 올리기 힘들지.";
+    }
+    void getScript_11()
+    {
+        tutorial_explanation.text = "시간이 0이 되면 데스매치가 된다. 추가 시간이 주어지고, 더 강력하게 공격할 수 있지.\n" +
+                                    "추가 시간 동안에도 승부를 보지 못하면, 힘을 더 많이 가한 사람이 판정 승을 챙긴다.";
     }
 
-    void getScript_9()
+    void getScript_12()
     {
         tutorial_explanation.text = "노력을 통해 승리를 쟁취하도록. (마우스 왼쪽 버튼 클릭시, 타이틀 화면으로 돌아갑니다.)";
     }
 
-    void getScript_10()
+    void getScript_13()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("TitleScene");
     }
---
+++ Assets/Script/TutorialScene/VectorManageScript.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorManageScript : MonoBehaviour
+{
+    float vector_1_x;
+    float vector_1_y;
+    float vector_2_x;
+    float vector_2_y;
+    public RectTransform vector_1;
+    public RectTransform vector_2;
+    public bool vector_1_enabled;
+    public bool vector_2_enabled;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vector_1_x = 0;
+        vector_1_y = 0;
+        vector_2_x = 0;
+        vector_2_y = 0;
+        vector_1_enabled = true;
+        vector_2_enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vector_1_enabled)
+            vector_1_input();
+        make_transform_from_vectors(vector_1, vector_1_x, vector_1_y);
+ 
+        if (vector_2_enabled)
+            vector_2_input();
+        make_transform_from_vectors(vector_2, vector_2_x, vector_2_y);
+    }
+
+    void vector_1_input()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (vector_2_x < 0)
+                vector_2_x += 1;
+            else vector_1_x -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (vector_2_x > 0)
+                vector_2_x -= 1;
+            else vector_1_x += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (vector_2_y < 0)
+                vector_2_y += 1;
+            else vector_1_y -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (vector_2_y > 0)
+                vector_2_y -= 1;
+            else vector_1_y += 1;
+        }
+    }
+    void vector_2_input()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (vector_1_x < 0)
+                vector_1_x += 1;
+            else vector_2_x -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (vector_1_x > 0)
+                vector_1_x -= 1;
+            else vector_2_x += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (vector_1_y < 0)
+                vector_1_y += 1;
+            else vector_2_y -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (vector_1_y > 0)
+                vector_1_y -= 1;
+            else vector_2_y += 1;
+        }
+    }
+
+    public float vector_length(float vector_x, float vector_y)
+    {
+        return Mathf.Pow((Mathf.Pow(vector_x, 2) + Mathf.Pow(vector_y, 2)), 0.5f);
+    }
+
+    public float vector_Angle(float vector_x, float vector_y)
+    {
+        return Mathf.Atan2(vector_y, vector_x);
+    }
+
+    void make_transform_from_vectors(RectTransform Target_Vector, float vector_x, float vector_y)
+    {
+        //백터 길이가 승리 길이일 때, scale이 1이면 된다.
+        float scale = 1f / 20 * vector_length(vector_x, vector_y);
+        Target_Vector.localScale = new Vector3(scale, scale, 0);
+
+        //백터 각도에 맞춰 Rotation을 돌린다.
+        float angle = vector_Angle(vector_x, vector_y) * Mathf.Rad2Deg;
+        Target_Vector.rotation = Quaternion.Euler(0, 0, angle);
+
+    }
+
+    public int check_winning()
+    {
+        if (vector_length(vector_1_x, vector_1_y) >= 20)
+            return 1;
+        if (vector_length(vector_2_x, vector_2_y) >= 20)
+            return 2;
+        return 0;
+    }
+    public bool check_p1_length_0()
+    {
+        if (vector_1_x == 0 && vector_1_y == 0)
+            return true;
+        return false;
+    }
+    public bool check_p2_length_0()
+    {
+        if (vector_2_x == 0 && vector_2_y == 0)
+            return true;
+        return false;
+    }
+
+    public void reset_game()
+    {
+        vector_1_x = 0;
+        vector_1_y = 0;
+        vector_2_x = 0;
+        vector_2_y = 0;
+    }
+    public void reset_game_p1()
+    {
+        vector_1_x = 0;
+        vector_1_y = 0;
+    }
+    public void reset_game_p2()
+    {
+        vector_2_x = 0;
+        vector_2_y = 0;
+    }
+    public void reset_game_2()
+    {
+        vector_1_x = 0;
+        vector_1_y = 0;
+        vector_2_x = 10;
+        vector_2_y = 10;
+    }
+    public void reset_game_3()
+    {
+        vector_1_x = 10;
+        vector_1_y = 10;
+        vector_2_x = 0;
+        vector_2_y = 0;
+    }
+}
